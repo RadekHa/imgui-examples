9733f3131c36b65ee4483ccde3036ee2cf0115d4
Radek Haščák <radek.hascak@sofa.cz>
2026-02-03 16:01:09 +0100

    Add msvc_sink_mt to log.

--- src/core/Core/Log.cpp
+++ src/core/Core/Log.cpp
@@ -3,30 +3,34 @@
 #include <spdlog/common.h>
 #include <spdlog/logger.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/msvc_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 #include <memory>
 #include <vector>
 
-namespace App {
+using namespace App;
+using namespace std;
+using namespace spdlog;
 
-Log::Log() {
-  std::vector<spdlog::sink_ptr> log_sinks;
+Log::Log ()
+{
+    vector<sink_ptr> log_sinks;
 
-  const spdlog::level::level_enum level{spdlog::level::debug};
+    const level::level_enum level{level::debug};
 
-  log_sinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-  log_sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("app.log", true));
+    log_sinks.emplace_back (make_shared<sinks::stdout_color_sink_mt>());
+    log_sinks.emplace_back (make_shared<sinks::msvc_sink_mt>());
+    log_sinks.emplace_back (make_shared<sinks::basic_file_sink_mt> ("app.log", true));
 
-  log_sinks[0]->set_pattern("%^[%T] %n(%l): %v%$");
-  log_sinks[1]->set_pattern("[%T] [%l] %n(%l): %v");
+    log_sinks [0]->set_pattern ("%^[%T] %n(%l): %v%$");
+    log_sinks [1]->set_pattern ("%^[%T] %n(%l): %v%$");
+    log_sinks [2]->set_pattern ("[%T] [%l] %n(%l): %v");
 
-  m_logger = std::make_shared<spdlog::logger>("APP", begin(log_sinks), end(log_sinks));
-  spdlog::register_logger(m_logger);
-  spdlog::set_default_logger(m_logger);
-  m_logger->set_level(level);
-  m_logger->flush_on(level);
+    m_logger = make_shared<spdlog::logger> ("APP", begin (log_sinks), end (log_sinks));
+    register_logger (m_logger);
+    set_default_logger (m_logger);
+    m_logger->set_level (level);
+    m_logger->flush_on (level);
 }
-
-}  // namespace App
