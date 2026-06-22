a7cc9183170a5e668ef6dc85d5aeb27154cc7213
Radek Haščák <radek.hascak@sofa.cz>
2026-06-22 20:37:26 +0200

    Add Ui states.

--- src/core/Core/AppUi.cpp
+++ src/core/Core/AppUi.cpp
@@ -4,9 +4,157 @@
 #include "imgui.h"
 
 #include <stdio.h>
+#include <string_view>
 
 using namespace App;
 using namespace Sdl;
+using namespace std;
+
+
+/** Declaration of the state interface. */
+class App::IUiState
+{
+public:
+    /** Virtual destructor to allow proper cleanup of derived classes. */
+    virtual ~IUiState () = default;
+    /** Update UI elements. */
+    virtual IUiState* update (DataModel& model, const SdlCameraTexture* camera) = 0;
+};
+
+/** Null state. */
+class StateNull : public IUiState
+{
+public:
+    /** {@inheritDoc} */
+    virtual IUiState* update (DataModel& model, const SdlCameraTexture* camera) override
+    {
+        return nullptr;
+    }
+};
+
+/** Login window. */
+class StateLogin : public IUiState
+{
+public:
+
+    /** {@inheritDoc} */
+    virtual IUiState* update (DataModel& model, const SdlCameraTexture* camera) override;
+
+private:
+    /** Check if the login is correct. */
+    bool isValid (string_view userName, string_view password) const;
+
+    /** User name value.*/
+    char m_username [64] = "";
+    /** Password value.*/
+    char m_password [64] = "";
+    /** Flag indicating that login is correct.*/
+    bool m_loginFailed{};
+};
+
+
+IUiState* StateLogin::update (DataModel& model, const SdlCameraTexture* camera)
+{
+    IUiState* state = nullptr;
+
+    constexpr string_view popupTitle = "Přihlášení do systému";
+
+    ImVec2 center = ImGui::GetMainViewport ()->GetCenter ();
+    ImGui::SetNextWindowPos (center, ImGuiCond_Appearing, ImVec2 (0.5f, 0.5f));
+    ImGui::SetNextWindowFocus ();
+
+    ImGui::OpenPopup (popupTitle.data ());
+
+    if (ImGui::BeginPopupModal (popupTitle.data (), nullptr, ImGuiWindowFlags_AlwaysAutoResize))
+    {
+        ImGui::Text ("Uživatelské jméno:");
+
+        if (ImGui::InputText ("##username", m_username, IM_ARRAYSIZE (m_username)))
+        {
+            m_loginFailed = false;
+        }
+        ImGui::Text ("Heslo:");
+
+        if (ImGui::InputText ("##password", m_password, IM_ARRAYSIZE (m_password), ImGuiInputTextFlags_Password))
+        {
+            m_loginFailed = false;
+        }
+        ImGui::Separator ();
+
+        if (ImGui::Button ("Přihlásit se", ImVec2 (120, 0)))
+        {
+            if (isValid (m_username, m_password))
+            {
+                ImGui::CloseCurrentPopup ();
+                state = new StateNull;
+            }
+            else
+            {
+                m_loginFailed = true;
+            }
+        }
+        ImGui::SetItemDefaultFocus ();
+
+        if (m_loginFailed)
+        {
+            ImGui::Spacing ();
+            ImGui::TextColored (ImVec4 (1, 0.3f, 0.3f, 1), "Neplatné přihlašovací údaje.");
+        }
+        ImGui::EndPopup ();
+    }
+    return state;
+}
+
+bool StateLogin::isValid (string_view userName, string_view password) const
+{
+    return userName == "user" && password == "pwd";
+}
+
+class StateStart : public IUiState
+{
+public:
+    /** {@inheritDoc} */
+    virtual IUiState* update (DataModel& model, const SdlCameraTexture* camera) override;
+};
+
+IUiState* StateStart::update (DataModel& model, const SdlCameraTexture* camera)
+{
+    IUiState* state = nullptr;
+
+    ImVec2 center = ImGui::GetMainViewport ()->GetCenter ();
+    ImGui::SetNextWindowPos (center, ImGuiCond_Appearing, ImVec2 (0.5f, 0.5f));
+    ImGui::SetNextWindowFocus ();
+
+    ImGui::OpenPopup ("Start");
+
+    if (ImGui::BeginPopupModal ("Start", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+    {
+        ImGui::Text ("All those beautiful files will be deleted.\nThis operation cannot be undone!");
+        ImGui::Separator ();
+
+        static bool dont_ask_me_next_time = false;
+
+        ImGui::PushStyleVar (ImGuiStyleVar_FramePadding, ImVec2 (0, 0));
+        ImGui::Checkbox ("Don't ask me next time", &dont_ask_me_next_time);
+        ImGui::PopStyleVar ();
+
+        if (ImGui::Button ("OK", ImVec2 (120, 0)))
+        {
+            ImGui::CloseCurrentPopup ();
+
+            state = new StateLogin;
+        }
+        ImGui::EndPopup ();
+    }
+    return state;
+}
+
+AppUi::AppUi ()
+    : m_state {new StateStart}
+{
+}
+
+AppUi::~AppUi () = default;
 
 void AppUi::update (DataModel& model, const SdlCameraTexture* camera)
 {
@@ -28,11 +176,6 @@ void AppUi::update (DataModel& model, const SdlCameraTexture* camera)
     if (model.showDemo)
     {
         ImGui::ShowDemoWindow (&model.showDemo);
-    }
-
-    if (model.showStats)
-    {
-        drawFpsGraph ();
     }
 
     if (camera && camera->isValid ())
@@ -59,6 +202,12 @@ void AppUi::update (DataModel& model, const SdlCameraTexture* camera)
                       ImVec2 (1, 0),
                       ImVec2 (0, 1));
         ImGui::End ();
+    }
+    IUiState* state = m_state->update (model, camera);
+
+    if (state != nullptr)
+    {
+        m_state.reset (state);
     }
 }
 
--- src/core/Core/AppUi.h
+++ src/core/Core/AppUi.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "DataModel.h"
 
+#include <memory>
+
 namespace Sdl
 {
     /** Forward declaration of sdl camera texture. */
@@ -9,15 +11,25 @@ namespace Sdl
 
 namespace App
 {
+    /** Forward declaration of the state interface. */
+    class IUiState;
+
     /** User Interface for the Application */
     class AppUi
     {
     public:
+        /** Initialize members. */
+        AppUi ();
+        /** Destructor for deleting an incomplete class pointer. */
+        ~AppUi ();
+
         /** Updates the UI based on the given data model. */
         void update (DataModel& model, const Sdl::SdlCameraTexture* camera);
 
     private:
         /** Draws a graph of the FPS over time. */
         void drawFpsGraph ();
+        /** Current state of the UI. */
+        std::unique_ptr<IUiState> m_state;
     };
 }
